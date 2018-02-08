de761d7d9a9c77e14ee5dfa748c7a48179c7c801
Zahaib Akhtar <zakhtar@conviva.com>
2018-02-07 21:20:46 -0800

    added the Google doc pipeline

---
+++ auto/macros.el
@@ -0,0 +1,36 @@
+(TeX-add-style-hook
+ "macros"
+ (lambda ()
+   (TeX-add-symbols
+    '("parae" 1)
+    '("subparagraph" 1)
+    '("nostarpara" 1)
+    '("starpara" 1)
+    '("starssubection" 1)
+    '("nostarsubsection" 1)
+    '("starsection" 1)
+    '("nostarsection" 1)
+    "sectionprelude"
+    "sectionpostlude"
+    "subsectionprelude"
+    "subsectionpostlude"
+    "paraspace"
+    "abraas"
+    "hyb"
+    "hybPred"
+    "bb"
+    "Dynamic"
+    "DynamicPP"
+    "DynamicMultiABR"
+    "convABR"
+    "sysname"
+    "Oracle"
+    "HomogBest"
+    "Cons"
+    "Mod"
+    "Aggr"
+    "ie"
+    "origsection"
+    "origsubsection"
+    "origparagraph")))
+

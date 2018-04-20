aac191ee0722af90db9f4d671f4b2b77c01dad55
George Papadimitriou <georgpap@isi.edu>
2018-04-20 01:26:20 -0700

    fetch additional m3u8

---
+++ crawler/work/www.cbssports.com/get_m3u8.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for file in *.m3u8; do
+    file=${file%%.*}.out
+    url=$( grep \.m3u8 $file | head -n 1)
+    url=${url##*?u=}
+    out=${file%%.*}
+    wget -O ${out}.m3u8 $url
+done
